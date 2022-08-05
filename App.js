fad3992f4e0a31422c87f10e018b3859c6da8c2b
Leandro Dantas <leandrosuy@gmail.com>
2022-08-05 12:57:33 -0300

    resolvendo problema da porta

--- App.js
+++ App.js
@@ -3,44 +3,45 @@ var app = express();
 var http = require("http").createServer(app);
 var fileSystem = require("fs");
 var fastcsv = require("fast-csv");
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(cors());
+
 app.use("/public", express.static(__dirname + "/public"));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 http.listen(port, function () {
-    console.log("Conectado!");
+    console.log("Connected");
 
     app.get("/baixarcsv", function (request, result) {
 
         var data = [{
             "id": 1,
             "name": "Leandro",
-            "age": 28
+            "age": 29
         }, {
             "id": 2,
             "name": "Eduardo",
-            "age": 28
+            "age": 31
         }, {
             "id": 3,
             "name": "Kerleston",
-            "age": 28
+            "age": 33
         },
         {
             "id": 4,
             "name": "Diego",
-            "age": 28
+            "age": 33
         }
         ];
 
-        var ws = fileSystem.createWriteStream("public/data.csv");
+        var ws = fileSystem.createWriteStream("public/data.txt");
         fastcsv
             .write(data, { headers: true })
-            .on("acabou!", function () {
+            .on("finish", function () {
 
-                result.send("<a href='/public/data.csv' download='data.csv' id='download-link'></a><script>document.getElementById('download-link').click();</script>");
+                result.send("<a href='/public/data.txt' download='data.txt' id='download-link'></a><script>document.getElementById('download-link').click();</script>");
             })
             .pipe(ws);
     });
