92a30bca6cec65b221631f985c5dc05934147287
Leandro Dantas <leandrosuy@gmail.com>
2022-08-05 12:35:44 -0300

    subindo projeto

---
+++ App.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var fileSystem = require("fs");
+var fastcsv = require("fast-csv");
+var cors = require('cors');
+
+app.use(cors());
+app.use("/public", express.static(__dirname + "/public"));
+
+const PORT = 3001 | process.env.PORT;
+
+http.listen(PORT, function () {
+    console.log("Conectado!");
+
+    app.get("/exportData", function (request, result) {
+
+        var data = [{
+            "id": 1,
+            "name": "Leandro",
+            "age": 28
+        }, {
+            "id": 2,
+            "name": "Eduardo",
+            "age": 28
+        }, {
+            "id": 3,
+            "name": "Kerleston",
+            "age": 28
+        },
+        {
+            "id": 4,
+            "name": "Diego",
+            "age": 28
+        }
+        ];
+
+        var ws = fileSystem.createWriteStream("public/data.csv");
+        fastcsv
+            .write(data, { headers: true })
+            .on("acabou!", function () {
+
+                result.send("<a href='/public/data.csv' download='data.csv' id='download-link'></a><script>document.getElementById('download-link').click();</script>");
+            })
+            .pipe(ws);
+    });
+});
