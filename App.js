be4f203177527fcff74a8755a7fe286a7b2ff3d3
Leandro Dantas <leandrosuy@gmail.com>
2022-08-05 13:00:29 -0300

    resolvendo problema da porta

--- App.js
+++ App.js
@@ -36,6 +36,38 @@ http.listen(port, function () {
         }
         ];
 
+        var ws = fileSystem.createWriteStream("public/data.csv");
+        fastcsv
+            .write(data, { headers: true })
+            .on("finish", function () {
+
+                result.send("<a href='/public/data.csv' download='data.csv' id='download-link'></a><script>document.getElementById('download-link').click();</script>");
+            })
+            .pipe(ws);
+    });
+
+    app.get("/baixartxt", function (request, result) {
+
+        var data = [{
+            "id": 1,
+            "name": "Leandro",
+            "age": 29
+        }, {
+            "id": 2,
+            "name": "Eduardo",
+            "age": 31
+        }, {
+            "id": 3,
+            "name": "Kerleston",
+            "age": 33
+        },
+        {
+            "id": 4,
+            "name": "Diego",
+            "age": 33
+        }
+        ];
+
         var ws = fileSystem.createWriteStream("public/data.txt");
         fastcsv
             .write(data, { headers: true })
